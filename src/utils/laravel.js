6769d4cdc887a7969722f9e575793c649f879491
kha333n <kha333n@gmail.com>
2024-03-24 03:39:30 +0500

    API Update

--- src/utils/laravel.js
+++ src/utils/laravel.js
@@ -1,19 +1,19 @@
-import axios from 'axios';
-
 const domain = 'https://api.kha333n.com/api';
 export const getAllPosts = async () => {
-  const data = await axios.get(domain + '/blogs');
-  return data.data.data;
+  const response = await fetch(domain + '/blogs');
+  const data = await response.json();
+  return data.data;
 };
 
 export const getPostBySlug = async (slug) => {
-  const data = await axios.get(domain + '/blogs/' + slug);
-  console.log('data', data.data.data);
-  return data.data.data;
+  const response = await fetch(domain + '/blogs/' + slug);
+  const data = await response.json();
+  return data.data;
 };
 
 export const findLatestPosts = async ({ count } = {}) => {
   const _count = count || 4;
-  const data = await axios.get(domain + '/latest/blogs');
-  return data.data.data;
+  const response = await fetch(domain + '/latest/blogs');
+  const data = await response.json();
+  return data.data;
 };
