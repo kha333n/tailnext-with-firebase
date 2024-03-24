aefdb30b382074c26ba2107d210f286ac78d87ef
kha333n <kha333n@gmail.com>
2024-03-24 17:47:42 +0500

    Favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en" className={`motion-safe:scroll-smooth 2xl:text-[24px] ${customFont.variable} font-sans`}>
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
-      <body className="tracking-tight antialiased text-gray-900 dark:text-slate-300">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="manifest" href="/manifest.json" />
+    </head>
+    <body className="tracking-tight antialiased text-gray-900 dark:text-slate-300">
         <Providers>
           {/*<Announcement />*/}
           <Header />
